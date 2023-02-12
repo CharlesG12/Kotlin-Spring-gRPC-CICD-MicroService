9c0950031600e6457d8db339b4bf65324ab71fb2
chengguo <cga78@sfu.ca>
2023-02-11 18:41:02 -0800

    add grpc service via armeria

--- Server/src/main/kotlin/com/example/demo/config/ArmeriaConfig.kt
+++ Server/src/main/kotlin/com/example/demo/config/ArmeriaConfig.kt
@@ -2,17 +2,20 @@ package com.example.demo.config
 
 import com.linecorp.armeria.server.cors.CorsService
 import com.linecorp.armeria.common.HttpMethod
+import com.linecorp.armeria.server.grpc.GrpcService
 import com.linecorp.armeria.server.logging.LoggingService
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import com.linecorp.armeria.server.logging.AccessLogWriter
+import io.grpc.BindableService
+import io.grpc.protobuf.services.ProtoReflectionService
 
 @Configuration
 class ArmeriaConfig {
 
     @Bean
-    fun armeriaServerConfigurator(): ArmeriaServerConfigurator =
+    fun armeriaServerConfigurator(bindableServices: List<BindableService>): ArmeriaServerConfigurator =
         ArmeriaServerConfigurator { builder ->
             builder
                 .decorator(LoggingService.builder().newDecorator())
@@ -22,5 +25,12 @@ class ArmeriaConfig {
                     .allowRequestHeaders("Accept", "Accept-Language", "Content-Language", "Content-Type")
                     .newDecorator())
                 .accessLogWriter(AccessLogWriter.combined(), false)
+                .service(
+                    GrpcService.builder()
+                        .addServices(bindableServices)
+                        .addService(ProtoReflectionService.newInstance())
+                        .enableUnframedRequests(true)
+                        .build()
+                )
         }
 }
